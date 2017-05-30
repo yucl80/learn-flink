7f7dd7de81ead3f8642f3c1d3521d1e42e090b25
yucl80 <ycl123>
2017-05-30 10:30:41 +0800

    init

---
+++ src/main/scala/yucl/learn/demo/AccLog.scala
@@ -0,0 +1,6 @@
+package yucl.learn.demo
+
+/**
+  * Created by YuChunlei on 2017/5/27.
+  */
+case class AccLog(system: String, sessionid: String, clientip: String, status: Double, bytes: Double, time: Double, timestamp: Long, count: Int, uri: String)
